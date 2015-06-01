485e01f52858cdc82cdf89b71a5abb4ad14d7c51
Anna <gaidanna@yandex.ru>
2015-06-01 23:45:59 +0300

    completed

---
+++ WorkersAmount/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGetInfo = new System.Windows.Forms.Button();
+            this.textBoxForPath = new System.Windows.Forms.TextBox();
+            this.textBoxForResult = new System.Windows.Forms.TextBox();
+            this.buttonBrowseFolder = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.textBoxSummary = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonGetInfo
+            // 
+            this.buttonGetInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGetInfo.Location = new System.Drawing.Point(362, 6);
+            this.buttonGetInfo.Name = "buttonGetInfo";
+            this.buttonGetInfo.Size = new System.Drawing.Size(75, 32);
+            this.buttonGetInfo.TabIndex = 1;
+            this.buttonGetInfo.Text = "Start";
+            this.buttonGetInfo.UseVisualStyleBackColor = true;
+            this.buttonGetInfo.Click += new System.EventHandler(this.OnButtonClick);
+            // 
+            // textBoxForPath
+            // 
+            this.textBoxForPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxForPath.Location = new System.Drawing.Point(13, 13);
+            this.textBoxForPath.Name = "textBoxForPath";
+            this.textBoxForPath.Size = new System.Drawing.Size(198, 20);
+            this.textBoxForPath.TabIndex = 0;
+            // 
+            // textBoxForResult
+            // 
+            this.textBoxForResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxForResult.Location = new System.Drawing.Point(13, 62);
+            this.textBoxForResult.Multiline = true;
+            this.textBoxForResult.Name = "textBoxForResult";
+            this.textBoxForResult.ReadOnly = true;
+            this.textBoxForResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxForResult.Size = new System.Drawing.Size(504, 284);
+            this.textBoxForResult.TabIndex = 2;
+            // 
+            // buttonBrowseFolder
+            // 
+            this.buttonBrowseFolder.Location = new System.Drawing.Point(217, 11);
+            this.buttonBrowseFolder.Name = "buttonBrowseFolder";
+            this.buttonBrowseFolder.Size = new System.Drawing.Size(30, 23);
+            this.buttonBrowseFolder.TabIndex = 3;
+            this.buttonBrowseFolder.Text = "...";
+            this.buttonBrowseFolder.UseVisualStyleBackColor = true;
+            this.buttonBrowseFolder.Click += new System.EventHandler(this.OnBrowseButtonClick);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Location = new System.Drawing.Point(443, 6);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 32);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Stop";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.OnCancelButtonClick);
+            // 
+            // textBoxSummary
+            // 
+            this.textBoxSummary.Location = new System.Drawing.Point(12, 368);
+            this.textBoxSummary.Multiline = true;
+            this.textBoxSummary.Name = "textBoxSummary";
+            this.textBoxSummary.ReadOnly = true;
+            this.textBoxSummary.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxSummary.Size = new System.Drawing.Size(504, 74);
+            this.textBoxSummary.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Processed information:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Summary:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 454);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSummary);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonBrowseFolder);
+            this.Controls.Add(this.textBoxForResult);
+            this.Controls.Add(this.textBoxForPath);
+            this.Controls.Add(this.buttonGetInfo);
+            this.Name = "Form1";
+            this.Text = "Optimal number of backgroundworkers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGetInfo;
+        private System.Windows.Forms.TextBox textBoxForPath;
+        private System.Windows.Forms.TextBox textBoxForResult;
+        private System.Windows.Forms.Button buttonBrowseFolder;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.TextBox textBoxSummary;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WorkersAmount/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        object locker;
+        int timeDifference;
+        List<string> folderList;
+        List<string> listOfDirectories = new List<string>();
+        List<TimeSpan> execusionTimeList = new List<TimeSpan>();
+        private List<BackgroundWorker> workersList = new List<BackgroundWorker>();
+        BackgroundWorker bgWorker = new BackgroundWorker();
+        BackgroundWorker bgWorkerForSubdirectories = new BackgroundWorker();
+        Stopwatch watch = new Stopwatch();
+
+        public Form1()
+        {
+            locker = new object();
+
+            InitializeComponent();
+            InitializeBackgroundWorker(bgWorker);
+            buttonCancel.Enabled = false;
+        }
+
+        private void InitializeBackgroundWorker(BackgroundWorker worker)
+        {
+            worker.DoWork += new DoWorkEventHandler(backgroundWorkerFiles_DoWork);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorkerFiles_RunWorkerCompleted);
+            worker.ProgressChanged += new ProgressChangedEventHandler(backgroundWorkerFiles_ProgressChanged);
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            workersList.Clear();
+            textBoxForResult.Clear();
+            textBoxSummary.Clear();
+            execusionTimeList.Clear();
+
+            if (Directory.Exists(textBoxForPath.Text) != true)
+            {
+                textBoxForResult.Text = "Path is invalid.";
+            }
+            else
+            {
+                buttonCancel.Enabled = true;
+                buttonGetInfo.Enabled = false;
+                buttonBrowseFolder.Enabled = false;
+
+                folderList = GetAllDirectories(textBoxForPath.Text);
+                listOfDirectories = folderList.ToList();
+                workersList.Add(bgWorker);
+                textBoxForResult.AppendText("1 worker\r\n");
+                watch.Restart();
+                workersList[0].RunWorkerAsync();
+            }
+        }
+
+        public List<string> GetAllDirectories(string path)
+        {
+            List<string> listOfDirectories;
+            listOfDirectories = new List<string>();
+
+            listOfDirectories = Directory.GetDirectories(path, "*", SearchOption.AllDirectories).ToList();
+            listOfDirectories.Add(path);
+
+            return listOfDirectories;
+        }
+
+        public string ConvertSize(double totalSize)
+        {
+            double byteEquivalent;
+            string lengthType;
+            double megaByte;
+            double gigaByte;
+
+            byteEquivalent = 1024;
+            megaByte = Math.Pow(byteEquivalent, 2);
+            gigaByte = Math.Pow(byteEquivalent, 3);
+
+            if (totalSize >= gigaByte)
+            {
+                totalSize = totalSize / gigaByte;
+                lengthType = totalSize.ToString("F") + " GB";
+            }
+            else if (totalSize >= megaByte)
+            {
+                totalSize = totalSize / megaByte;
+                lengthType = totalSize.ToString("F") + " MB";
+
+            }
+            else if (totalSize >= byteEquivalent)
+            {
+                totalSize = totalSize / byteEquivalent;
+                lengthType = totalSize.ToString("F") + " KB";
+            }
+            else
+            {
+                lengthType = totalSize.ToString() + " bytes";
+            }
+            return lengthType;
+        }
+
+
+        public string GetAllExtentionsOfFiles(string directory)
+        {
+            List<string> FolderInfo;
+            List<string> ListOfExtensions;
+            string[] fileEntries;
+
+            FolderInfo = new List<string>();
+            ListOfExtensions = new List<string>();
+            long size = 0;
+
+            fileEntries = Directory.GetFiles(directory);
+            ListOfExtensions.Sort();
+
+            if (fileEntries.Length == 0)
+            {
+                return string.Format("Folder - {0}\r\nEmpty\r\n", directory);
+            }
+            else
+            {
+                for (int i = 0; i < fileEntries.Length; i++)
+                {
+                    size += new FileInfo(fileEntries[i]).Length;
+                }
+                return string.Format("Folder - {0}\r\nNumber of files - {1}, total size - {2}\r\n", directory, fileEntries.Length, ConvertSize((double)size));
+            }
+        }
+
+        private void backgroundWorkerFiles_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string folderPath;
+            List<string> ListToReportExtentions = new List<string>();
+
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            while (folderList.Count > 0)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                lock (folderList)
+                {
+                    folderPath = folderList[0];
+                    folderList.RemoveAt(0);
+                }
+                (sender as BackgroundWorker).ReportProgress(0, GetAllExtentionsOfFiles(folderPath));
+            }
+        }
+
+        private void backgroundWorkerFiles_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string line;
+            lock (locker)
+            {
+                line = (string)e.UserState;
+
+                textBoxForResult.AppendText(line + Environment.NewLine);
+            }
+        }
+
+        private void backgroundWorkerFiles_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                textBoxSummary.Text = "Canceled!";
+                buttonGetInfo.Enabled = true;
+                buttonCancel.Enabled = false;
+                buttonBrowseFolder.Enabled = true;
+
+            }
+            else
+            {
+                bool StopWatchCount;
+                int AmountOfBG;
+                StopWatchCount = true;
+
+                foreach (BackgroundWorker BG in workersList)
+                {
+                    if (BG.IsBusy)
+                    {
+                        StopWatchCount = false;
+                        break;
+                    }
+                }
+
+                if (StopWatchCount)
+                {
+                    watch.Stop();
+                    var ts = TimeSpan.FromMilliseconds(watch.ElapsedMilliseconds);
+                    execusionTimeList.Add(ts);
+                    timeDifference = 1;
+
+                    if (execusionTimeList.Count > 1)
+                    {
+                        timeDifference = TimeSpan.Compare(execusionTimeList[execusionTimeList.Count - 2], execusionTimeList[execusionTimeList.Count - 1]);
+                    }
+
+                    if (timeDifference == 1)
+                    {
+                        folderList = listOfDirectories.ToList();
+                        BackgroundWorker NewBGWorker = new BackgroundWorker();
+                        workersList.Add(NewBGWorker);
+                        textBoxForResult.AppendText(string.Format("{0} workers\r\n", workersList.Count));
+                        InitializeBackgroundWorker(NewBGWorker);
+                        watch.Restart();
+
+                        for (int a = 0; a < workersList.Count; a++)
+                        {
+                            workersList[a].RunWorkerAsync();
+                        }
+                    }
+                    else
+                    {
+                        if (workersList.Count > 1)
+                        {
+                            AmountOfBG = workersList.Count - 1;
+
+                            for (int i = 0; i < execusionTimeList.Count; i++)
+                            {
+                                textBoxSummary.AppendText(String.Format("Time for {0} BG - {1:D2}m:{2:D2}s:{3:D2}ms\r\n", (i + 1), execusionTimeList[i].Minutes, execusionTimeList[i].Seconds, execusionTimeList[i].Milliseconds));
+                            }
+
+                            textBoxSummary.AppendText("Optimal amount of BG -" + (AmountOfBG.ToString()) + Environment.NewLine);
+                        }
+                        else
+                        {
+                            textBoxSummary.AppendText("Optimal amount of BG is 1. " + Environment.NewLine);
+                        }
+                        buttonGetInfo.Enabled = true;
+                        buttonCancel.Enabled = false;
+                        buttonBrowseFolder.Enabled = true;
+                    }
+                }
+            }
+        }
+
+        private void OnBrowseButtonClick(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.SelectedPath = textBoxForPath.Text;
+                folderDialog.Description = "Please select a folder";
+                folderDialog.ShowNewFolderButton = false;
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxForPath.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void OnCancelButtonClick(object sender, EventArgs e)
+        {
+            foreach (var worker in workersList)
+            {
+                worker.CancelAsync();
+            }
+        }
+    }
+}
+
+
+
